9e664ea6a063a9b19a4af89dcd4b9d575b1e6787
Irene Liampa <irini.liampa@gmail.com>
2018-11-12 14:40:27 +0200

    Update dependencies.R

--- dependencies.R
+++ dependencies.R
@@ -1,6 +1,6 @@
 update.packages()
 
-if (!requireNamespace("BiocManager", quietly = TRUE)){
+if (!requireNamespace("BiocManager", quietly = TRUE))
 	install.packages("BiocManager")
 	BiocManager::install()
 	BiocManager::install('remotes')
@@ -10,5 +10,4 @@ if (!requireNamespace("BiocManager", quietly = TRUE)){
 	BiocManager::install("biomaRt", version = "3.8", update = TRUE, ask = FALSE)
 	BiocManager::install('rafalab/bumphunter', ref = "RELEASE_3_8")
 	BiocManager::install("minfi", version = "3.8", update = TRUE, ask = FALSE)
-	BiocManager::install("conumee", version = "3.8", update = TRUE, ask = FALSE)	
-}
+	BiocManager::install("conumee", version = "3.8", update = TRUE, ask = FALSE)
